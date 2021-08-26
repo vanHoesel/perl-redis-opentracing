1da0e3466de5bed904e877214187a2912d7bbd22
Theo van Hoesel <tvanhoesel@perceptyx.com>
2021-08-26 11:46:38 +0200

    Add `AUTOLOAD` to capture and dispatch methods to injected redis client

--- lib/Redis/OpenTracing.pm
+++ lib/Redis/OpenTracing.pm
@@ -14,5 +14,17 @@ sub _build_redis {
     Redis->new
 }
 
+our $AUTOLOAD; # keep 'use strict' happy
+
+sub AUTOLOAD {
+    my $self = shift;
+    
+    my $method_call = do { $_ = $AUTOLOAD; s/.*:://; $_ };
+    
+    return $self->redis->$method_call(@_)
+}
+
+
+sub DESTROY { } # we don't want this to be dispatched
 
 1;
